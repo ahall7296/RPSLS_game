d68b146d9238ae40b58d2be6efc4883bdb90b734
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-01-31 21:09:29 -0500

    Added parameters for the player class

--- player.py
+++ player.py
@@ -1,1 +1,12 @@
 class Player:
+
+    def __init__(self):
+        self.name = ""
+        self.score = 0
+        self.gesture_list = ["Rock, Paper, Scissors, Lizard, Spock"]
+
+    def set_name(self):
+        pass
+
+    def choose_gesture(self):
+        pass
