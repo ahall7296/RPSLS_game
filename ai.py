5c61b6c02876c42a9a30da1c2577b365a007e25c
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-02-05 21:10:30 -0500

    Added parameters for the AI class

--- ai.py
+++ ai.py
@@ -1,1 +1,10 @@
+from player import Player
+import random
+
 class AI(Player):
+    def __init__(self):
+        self.name = "Player 2"
+
+    def choose_gesture(self):
+        bot_selects = random.choice(self.choice)
+        return bot_selects
