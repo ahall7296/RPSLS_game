3d27ac7e35f7f18c4bd2792cf49c51c2a72e3c24
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-02-05 21:05:59 -0500

    Added parameters for the human class and updated player class.

--- human.py
+++ human.py
@@ -1,1 +1,8 @@
+from player import Player
+
 class Human(Player):
+    def __init__(self, name):
+        self.name = name
+
+    def choose_gesture(self):
+        self.selected_choice = self.gesture_list
