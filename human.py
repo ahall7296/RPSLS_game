8ac1f5027c4c2c82dda860c9ff851856d6e300f9
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-02-07 15:38:08 -0500

    Added op[tion to select choices for human player.

--- human.py
+++ human.py
@@ -5,4 +5,10 @@ class Human(Player):
         self.name = name
 
     def choose_gesture(self):
-        selected_choice = self.gesture_list
+        selected_choice = self.gesture_list[(int(input(f'''Choose your gesture, 1-5: 
+        1: {self.choice[0]}
+        2: {self.choice[1]}
+        3: {self.choice[2]}
+        4: {self.choice[3]}
+        5: {self.choice[4]}
+        ''')))-1]
